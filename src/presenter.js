60251f20e22031356a7a4869e4f9bd69d686a481
Adrian Claros <166774927+AdrianCV2345@users.noreply.github.com>
2025-08-21 09:14:24 -0400

    presenter: funcionalidad saludo por genero. index.html: formulario por seleccion de genero

--- src/presenter.js
+++ src/presenter.js
@@ -3,19 +3,15 @@ import sumar from "./sumador";
 const nombre = document.querySelector("#nombre");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
-const divSaludoHora = document.querySelector("#saludo-hora-div");
+const saludoGenero = document.querySelector("#genero");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const hora = new Date().getHours();
-  if (hora < 12) {
-    divSaludoHora.innerHTML = "<p>¡Buenos días!<p>";
-  } else if (hora < 18) {
-    divSaludoHora.innerHTML = "<p>¡Buenas tardes!<p>";
-  } else {
-    divSaludoHora.innerHTML = "<p>¡Buenas noches!<p>";
-  }
-    div.innerHTML = "<p> " + nombre.value + "!<p>";
-
+    const genero = saludoGenero.value;
+    if (genero === "masculino") {
+      div.innerHTML += "¡Hola, caballero! " + nombre.value;
+    } else {
+      div.innerHTML += "¡Hola, dama! " + nombre.value;
+    }
 });
  
