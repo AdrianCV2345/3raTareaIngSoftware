0162a4f0f6e34b370705298e7157575a8b275118
Adrian Claros <166774927+AdrianCV2345@users.noreply.github.com>
2025-08-21 08:41:59 -0400

    Saludador segun la hora listo.

--- src/presenter.js
+++ src/presenter.js
@@ -3,8 +3,19 @@ import sumar from "./sumador";
 const nombre = document.querySelector("#nombre");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
+const divSaludoHora = document.querySelector("#saludo-hora-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  div.innerHTML = "<p>Hola, " + nombre.value + "!</p>";
+  const hora = new Date().getHours();
+  if (hora < 12) {
+    divSaludoHora.innerHTML = "<p>¡Buenos días!<p>";
+  } else if (hora < 18) {
+    divSaludoHora.innerHTML = "<p>¡Buenas tardes!<p>";
+  } else {
+    divSaludoHora.innerHTML = "<p>¡Buenas noches!<p>";
+  }
+    div.innerHTML = "<p> " + nombre.value + "!<p>";
+
 });
+ 
